4ed3e44ba1bed60ef27a9f6b2d5be25339042184
Natalie Denisova <32077275+fearisinternal@users.noreply.github.com>
2017-10-10 20:30:41 +0300

    Add files via upload

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -7,9 +7,9 @@ double eps = 1e-8;
 bool Complex::operator==(const Complex& rhs) { return (re - rhs.re < eps) && (im - rhs.im < eps); }
 bool Complex::operator!=(const Complex& rhs) { return !operator==(rhs); }
 
-//Complex& Complex::operator+=(const Complex& rhs) {
-//    return Complex((re + rhs.re), (im + rhs.im));
-//}
+Complex& Complex::operator+=(const Complex& rhs) {
+    return Complex((re + rhs.re), (im + rhs.im));
+}
 //Complex& Complex::operator-=(const Complex& rhs) {
 //    return Complex((re - rhs.re), (im - rhs.im));
 //}
