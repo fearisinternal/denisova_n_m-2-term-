dc1c1437b69306a9acc199c3b9570256e2fe3dfa
Natalie Denisova <32077275+fearisinternal@users.noreply.github.com>
2017-11-09 11:24:32 +0300

    Add files via upload

--- array/array.cpp
+++ array/array.cpp
@@ -3,7 +3,7 @@
 
 void Array::add() {
     if (size_ < capacity_) {
-        ++size_;
+        size_++;
     }
     else {
         int* new_pdata = new int[capacity_ * 2];
@@ -19,3 +19,31 @@ void Array::add() {
 int Array::size() const { 
     return size_; 
 }
+
+void Array::resize(const int& a) {
+	if (a >= 0) {
+		size_ = a;
+		capacity_ = a * 2;
+	}
+}
+
+void Array::insert(const int& a, const int& new_i) {
+    if (size_ >= capacity_) {
+        capacity_ *= 2;
+    }
+    int* new_pdata = new int[capacity_];
+    for (int i = 0; i < new_i; i++) {
+        *(new_pdata + i) = *(pdata_ + i);
+    }
+    for (int i = size_; i >i-1; i--) {
+        *(new_pdata + i) = *(pdata_ + i-1);
+    }
+    *(new_pdata + new_i) = a;
+    size_++;
+    std::swap(new_pdata, pdata_);
+}
+
+int& Array::operator[](const ptrdiff_t i) {
+    if (i>=0 && i<size_) return pdata_[i];
+}
+
